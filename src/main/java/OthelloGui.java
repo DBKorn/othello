1277f6cd1b3418f9c3c0fe970b7d554f19d9041e
DBKorn <70672608+DBKorn@users.noreply.github.com>
2024-02-26 15:29:45 -0500

    Update OthelloGui

--- src/main/java/OthelloGui.java
+++ src/main/java/OthelloGui.java
@@ -1,1 +1,71 @@
 
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OthelloGUI extends JFrame {
+
+    private OthelloModel model = new OthelloModel();
+    private JButton[][] buttons;
+    private int boardDimensionHeight = 520, boardDimensionWidth = 500;
+    int boardSize = model.boardSize;
+
+
+    public OthelloGUI(){
+        //this.model = model;
+        //model = new OthelloModel();
+        setTitle("Othello");
+        this.setSize(boardDimensionWidth, boardDimensionHeight);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+
+        this.setLayout(new GridLayout(boardSize, boardSize));
+
+        ActionListener a = new ButtonListener();
+        buttons = new MyJButton[boardSize][boardSize];
+
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                this.add(buttons[i][j] = new MyJButton(i,j));
+                buttons[i][j].addActionListener(a);
+                buttons[i][j].setBackground(Color.GREEN);
+            }
+        }
+        updateBoard();
+    }
+
+    void updateBoard(){
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                OthModInter.GamePiece g = model.whatsInThatSquare(i,j);
+                buttons[i][j].setText(g.toIcon());
+            }
+        }
+    }
+
+    private class ButtonListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            MyJButton m = (MyJButton) e.getSource();
+            updateBoard();
+            model.placePiece(model.whosTurn(), m.x, m.y);
+
+            updateBoard();
+        }
+    }
+
+    private static class MyJButton extends JButton {
+        int x,y;
+        MyJButton(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+
+}
